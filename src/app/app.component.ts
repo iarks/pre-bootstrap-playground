f89bae241420bd63298aeb99a0dab0315b43fa1a
Arkadeep Saha <arkadeepsaha11@gmail.com>
2020-04-26 01:46:43 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pre-bootstrap-playground';
+
+  private doc: Document;
+
+
+	// I initialize the service.
+	// --
+	// NOTE: When I first tried to approach this problem, I was going to try and use the
+	// core Renderer service; however, it appears that the Renderer cannot be injected
+	// into a service object (throws error: No provider for Renderer!). As such, I am
+	// treating THIS class as the implementation of the DOM abstraction (so to speak),
+	// which can be overridden on a per-environment basis.
+	constructor( @Inject( DOCUMENT ) doc: any ) {
+    let event = new Event("appready");
+    doc.dispatchEvent(event);
+  }
+  
+  
 }
